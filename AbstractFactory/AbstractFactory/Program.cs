2a7eae518d302e9ee2dcdb9d21c20bbc1dc8c7e9
Nitin Vaswani <nvaswani@infoideashealth.onmicrosoft.com>
2017-10-05 18:38:34 +0530

    first commit

---
+++ AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    interface IDumb
+    {
+        string Name();
+    }
+    interface ISmart
+    {
+        string Name();
+    }
+    class Asha : IDumb
+    {
+
+        public string Name()
+        {
+            return "Asha";
+        }
+    }
+
+    class Genie:IDumb
+    {
+        public string Name()
+        {
+            return "Genie";
+        }
+    }
+    class Primo : IDumb
+    {
+        public string Name()
+        {
+            return "Primo";
+        }
+    }
+    class Lumia:ISmart
+    {
+
+        public string Name()
+        {
+            return "Lumia";
+        }
+    }
+
+    class GaalxyS2 : ISmart
+    {
+
+        public string Name()
+        {
+            return "GaalxyS2";
+        }
+    }
+
+    class Duos :ISmart
+    {
+        public string Name()
+        {
+            return "Duos";
+        }
+    }
+
+    class Titan:ISmart
+    {
+        public string Name()
+        {
+            return "Titan";
+        }
+    }
+
+    interface iPhoneFactory
+    {
+        ISmart GetSmart();
+        IDumb GetDumb();
+    }
+
+    class SamsungFactory:iPhoneFactory
+    {
+
+        public ISmart GetSmart()
+        {
+            return new GaalxyS2();
+        }
+
+        public IDumb GetDumb()
+        {
+            return new Genie();
+        }
+    }
+    class NokiaFactory : iPhoneFactory
+    {
+
+        public ISmart GetSmart()
+        {
+          return new Lumia();
+        }
+
+        public IDumb GetDumb()
+        {
+            return new Asha();
+        }
+    }
+
+    class HTCFactory:iPhoneFactory
+    {
+
+        public ISmart GetSmart()
+        {
+          return new Titan();
+        }
+
+        public IDumb GetDumb()
+        {
+            return new Primo();
+        }
+    }
+    enum MANUFACTURERS
+    {
+        SAMSUNG,
+        HTC,
+        NOKIA
+    }
+    class PhoneTypeChecker
+{
+    ISmart sam;
+    IDumb htc;
+    iPhoneFactory factory;
+    MANUFACTURERS manu;
+
+    public PhoneTypeChecker(MANUFACTURERS m)
+    {
+        manu = m;
+    }
+
+    public void CheckProducts()
+    {
+        switch (manu)
+        {
+            case MANUFACTURERS.SAMSUNG:
+                factory = new SamsungFactory();
+                break;
+            case MANUFACTURERS.HTC:
+                factory = new HTCFactory();
+                break;
+            case MANUFACTURERS.NOKIA:
+                factory = new NokiaFactory();
+                break;
+        }
+
+        Console.WriteLine(manu.ToString() + ":\nSmart Phone: " +
+        factory.GetSmart().Name() + "\nDumb Phone: " + factory.GetDumb().Name());
+    }
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneTypeChecker checker = new PhoneTypeChecker(MANUFACTURERS.SAMSUNG);
+
+            checker.CheckProducts();
+
+            Console.ReadLine();
+
+            checker = new PhoneTypeChecker(MANUFACTURERS.HTC);
+
+            checker.CheckProducts();
+            Console.ReadLine();
+
+            checker = new PhoneTypeChecker(MANUFACTURERS.NOKIA);
+
+            checker.CheckProducts();
+            Console.Read();
+        }
+    }
+}
---
+++ FactoryMethod_GangOfFour/FactoryMethod_GangOfFour/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod_GangOfFour
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Document[] documents = new Document[2];
+            documents[0] = new Resume();
+            documents[1] = new Book();
+            foreach (Document doc in documents)
+            {
+                Console.WriteLine("Document is:-"+doc.GetType().Name+"---");
+                foreach (Page page in doc.Pages)
+                {
+                    Console.WriteLine("\t\t" + page.GetType().Name);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class Page
+    { }
+
+    public class IntroductionPage : Page
+    { }
+
+    public class SkillsPage : Page
+    { }
+
+    public class ExperiencePage : Page
+    { }
+
+    public class EducationPage :Page
+    { }
+
+    public class SummaryPage :Page
+    {}
+
+    public class IndexPage : Page
+    { }
+
+    public class BibloGraphyPage: Page
+    { }
+
+    public class conslusionPage : Page
+    { }
+
+    public class AppendexPage : Page
+    { }
+
+    public abstract class Document
+    {
+        public List<Page> Pages = new List<Page>();
+
+        public Document()
+        {
+            this.CreatePages();
+        }
+
+
+        public abstract void CreatePages();
+
+    }
+
+    public class Resume:Document
+    {
+
+        public override void CreatePages()
+        {
+            Pages.Add(new IntroductionPage());
+            Pages.Add(new SkillsPage());
+            Pages.Add(new EducationPage());
+            Pages.Add(new ExperiencePage());
+        }
+    }
+
+    public class Book:Document
+    {
+        public override void CreatePages()
+        {
+            Pages.Add(new IntroductionPage());
+            Pages.Add(new BibloGraphyPage());
+            Pages.Add(new IndexPage());
+            Pages.Add(new SummaryPage());
+            Pages.Add(new conslusionPage());
+            Pages.Add(new AppendexPage());
+        }
+
+    }
+
+}
